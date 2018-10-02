3a59018734a770942cac3422790de09c3519ce11
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2018-10-02 13:36:41 -0400

    Add files via upload

---
+++ RectJawn.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RectJawn {
+
+	public static void main(String[] args) {
+	
+	Scanner scanner1 = new Scanner(System.in);
+	
+	double length;
+	double width;
+	
+	
+	
+
+	}
+
+}
