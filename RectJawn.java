80c2d2687b8b48f3704ccde3e4b63901418cbf7b
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2018-10-02 22:10:38 -0400

    Add files via upload

--- RectJawn.java
+++ RectJawn.java
@@ -4,12 +4,30 @@ public class RectJawn {
 
 	public static void main(String[] args) {
 	
-	Scanner scanner1 = new Scanner(System.in);
-	
-	double length;
-	double width;
+	Scanner in = new Scanner(System.in);
 	
 	
+    System.out.print("Length:  ");
+    
+    double length = in.nextDouble();
+    
+    in.nextLine();
+    
+    System.out.print("Width:  ");
+    
+    double width = in.nextDouble();
+    
+    System.out.println();
+    
+    System.out.printf("Area:      %10.2f", length*width);
+    
+    System.out.println();
+    
+    System.out.printf("Perimeter: %10.2f", 2*(length+width));
+    
+    System.out.println();
+    
+    System.out.printf("Diagonal:  %10.2f", Math.sqrt(length*length + width*width));
 	
 
 	}
