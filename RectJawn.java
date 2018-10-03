ab5e6e340987f086952f147ff844231a534ce27d
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2018-10-03 13:03:29 -0400

    Update RectJawn.java

--- RectJawn.java
+++ RectJawn.java
@@ -4,32 +4,33 @@ public class RectJawn {
 
 	public static void main(String[] args) {
 	
-	Scanner in = new Scanner(System.in);
+	Scanner scanner1 = new Scanner(System.in);
 	
 	
-    System.out.print("Length:  ");
+    System.out.print("Please enter the Length:  ");
     
-    double length = in.nextDouble();
+    double length = scanner1.nextDouble();
     
-    in.nextLine();
+    	scanner1.nextLine();
     
-    System.out.print("Width:  ");
+    System.out.print("Please enter the Width:  ");
     
-    double width = in.nextDouble();
-    
-    System.out.println();
-    
-    System.out.printf("Area:      %10.2f", length*width);
+    double width = scanner1.nextDouble();
     
     System.out.println();
     
-    System.out.printf("Perimeter: %10.2f", 2*(length+width));
+    System.out.printf("Area:      %10.2f",length * width);
     
     System.out.println();
     
-    System.out.printf("Diagonal:  %10.2f", Math.sqrt(length*length + width*width));
+    System.out.printf("Perimeter: %10.2f",2*(length + width));
+    
+    System.out.println();
+    
+    System.out.printf("Diagonal:  %10.2f",Math.sqrt(length * length + width * width));
 	
 
 	}
 
 }
+ 
